8b519277614170e6f37a09136007f4fca604de69
Apipilikas <apipilikas@gmail.com>
2022-05-02 21:40:53 +0300

    added changes

---
+++ app/src/main/java/com/aueb/idry/T8816WP/FiltersGuideSecondActivity.java
@@ -0,0 +1,16 @@
+package com.aueb.idry.T8816WP;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aueb.idry.R;
+
+public class FiltersGuideSecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filters_guide_second);
+    }
+}
