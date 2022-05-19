bcc1aa53afc7fff216e9be1a7aec13328a890089
AnAthenianOwl <costasvas34@protonmail.com>
2022-05-20 00:15:36 +0300

    main activity basic layout added

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -3,14 +3,35 @@ package activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.aueb.idry.R;
 
 public class MainActivity extends AppCompatActivity {
+    private final int TEXT_GROW_THRESHOLD = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Resize the start button's text if it's too big
+        Button mainStartBtn = findViewById(R.id.mainStartBtnLabel);
+        if (mainStartBtn.getText().length() < TEXT_GROW_THRESHOLD) {
+            // Grow to 24sp
+            mainStartBtn.setTextSize(24);
+        }
+
+        // Round the corners of the notifications' layout by allowing it to clip its corners
+        LinearLayout notificationsLayout = findViewById(R.id.mainNotificationsLayout);
+        notificationsLayout.setClipToOutline(true);
+
+        // TODO: Get the dryer interface
+
+        // TODO: Create notification fragments if required
+        // TODO: Filters notification
+
+        // TODO: Container notification
     }
 }
