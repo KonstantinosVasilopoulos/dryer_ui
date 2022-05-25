9a3c6914b53121652b3236fb75f9e4bbd30b57e7
01iverr <cchrhstos@yahoo.com>
2022-05-26 00:44:34 +0300

    settings updated

--- app/src/main/java/activity/Settings.java
+++ app/src/main/java/activity/Settings.java
@@ -2,6 +2,7 @@ package activity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,42 +10,107 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.aueb.idry.R;
 
 public class Settings extends AppCompatActivity {
-
-    Switch NotifSwitcher = findViewById(R.id.switchsettnot);
-    Switch VoiceInstrSwitcher = findViewById(R.id.switchsettVI);
-    Switch VoiceCommandsSwitcher =findViewById(R.id.switchSettVC);
-    Switch EnglishSwitcher = findViewById(R.id.switchsetEN);
-    Switch GreekSwitcher = findViewById(R.id.switchSettGr);
+    boolean notification;
+    boolean VOICE_INSTRUCTION;
+    boolean VOICE_COMMANDS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        @SuppressLint("UseSwitchCompatOrMaterialCode")
+        Switch NotifSwitcher = findViewById(R.id.switchsettnot);
+        @SuppressLint("UseSwitchCompatOrMaterialCode")
+        Switch VoiceInstrSwitcher = findViewById(R.id.switchsettVI);
+        @SuppressLint("UseSwitchCompatOrMaterialCode")
+        Switch VoiceCommandsSwitcher = findViewById(R.id.switchSettVC);
+        @SuppressLint("UseSwitchCompatOrMaterialCode")
+        Switch EnglishSwitcher = findViewById(R.id.switchsetEN);
+        @SuppressLint("UseSwitchCompatOrMaterialCode")
+        Switch GreekSwitcher = findViewById(R.id.switchSettGr);
+        Button returnn = findViewById(R.id.returnfromsettings);
+        super.onCreate(savedInstanceState);
         NotifSwitcher.setChecked(true);
         VoiceInstrSwitcher.setChecked(false);
         VoiceCommandsSwitcher.setChecked(false);
-        GreekSwitcher.setChecked(true);
-        EnglishSwitcher.setChecked(false);
+        GreekSwitcher.setChecked(false);
+        EnglishSwitcher.setChecked(true);
+
+        /* ENGLISH SWITCER */
+        EnglishSwitcher.setOnClickListener(view -> {
+            if (EnglishSwitcher.isChecked()) {
+                GreekSwitcher.setChecked(false);
+                lan( true );
+            }else{
+                GreekSwitcher.setChecked(true);
+                lan( false);
+            }
+        });
+
+        /* GREEK SWITCER */
+        GreekSwitcher.setOnClickListener(view -> {
+            if (GreekSwitcher.isChecked()){
+                EnglishSwitcher.setChecked(false);
+                lan( false);
+            }
+            else{
+                EnglishSwitcher.setChecked(true);
+                lan( true);
+            }
+        });
+
+        /* NOTIFICATIONS SWITCER */
+        NotifSwitcher.setOnClickListener(view -> setNotification(NotifSwitcher.isChecked()));
+
+        /* VOICE INSTRUCTIONS SWITCER */
+        VoiceInstrSwitcher.setOnClickListener(view -> setVOICE_INSTRUCTION(VoiceInstrSwitcher.isChecked()));
+
+        /* VOICE COMMANDS SWITCER */
+        VoiceCommandsSwitcher.setOnClickListener(view -> {
+            setVOICE_COMMANDS(VoiceCommandsSwitcher.isChecked());
+        });
+
+
+        /* returnn button */
+        returnn.setOnClickListener(view -> {
+           //go in the previus page
+        });
     }
 
-//    private void LanguageButtons(){
-//        if (GreekSwitcher.isChecked()){
-//            EnglishSwitcher.setChecked(false);
-//        }
-//        else if(EnglishSwitcher.isChecked()) {
-//            GreekSwitcher.setChecked(false);
-//        }
-//    }
-//    public boolean NotificationsOn(){
-//        return NotifSwitcher.isChecked();
-//    }
-//
-//    public boolean VoiceInstructionOn(){
-//        return VoiceInstrSwitcher.isChecked();
-//    }
-//
-//    public boolean VoiceCommnadsOn(){
-//        return VoiceCommandsSwitcher.isChecked();
-//    }
+    private void lan( boolean b ) {
+        /* change language? */
+        if (b) {
+            LanguageHelper.setLocale(this, "en");
+        } else {
+            LanguageHelper.setLocale(this , "gr");
+        }
+    }
+
+    public boolean NotificationOn(){
+        return notification;
+    }
+
+    private void setNotification(boolean a){
+        notification=a;
+    }
+
+    private void setVOICE_INSTRUCTION(boolean a) {
+        VOICE_INSTRUCTION = a;
+    }
+
+    public boolean VOICE_INSTRUCTIONOn(){
+        return VOICE_INSTRUCTION;
+    }
+
+    public boolean VOICE_COMMANDSOn(){
+        return VOICE_COMMANDS;
+    }
+
+    private void setVOICE_COMMANDS(boolean a) {
+        VOICE_COMMANDS = a;
+    }
 
 }
+
+
+
+
 
