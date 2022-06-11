d13b282982719d15ca3bc83b4041b544c5e9fe06
Konstantinos Vasilopoulos <costasvas34@protonmail.com>
2022-06-11 19:09:02 +0300

    fixed typos 02

--- app/src/main/java/activity/FunctionButtonsFragment.java
+++ app/src/main/java/activity/FunctionButtonsFragment.java
@@ -76,25 +76,14 @@ public class FunctionButtonsFragment extends Fragment {
             }
         });
 
-
-        //Home Button
-        ImageButton HomeBtn;
-        HomeBtn = view.findViewById(R.id.homeBtn);
-        HomeBtn.setOnClickListener(v -> {
-            // Navigate to the first page with favorites programs
+        // Home button
+        ImageButton homeBtn = view.findViewById(R.id.homeBtn);
+        homeBtn.setOnClickListener(v -> {
+            // Navigate to the routines menu activity
             if (getActivity() != null) {
                 startActivity(new Intent(getActivity(), RoutineMenuActivity.class));
             }
         });
-
-//        if (this.getClass().getSimpleName().contains("SelectionFirstStepActivity")
-//                || this.getClass().getSimpleName().contains("SelectionSecondStepActivity")
-//                || this.getClass().getSimpleName().equals("SelectionThirdStepActivity")){
-//            HomeBtn.setVisibility(View.VISIBLE);
-//        } else{
-//            HomeBtn.setVisibility(View.GONE);}
-
-
 
         // Door unlock button
         doorUnlockBtn.setOnClickListener(v -> {
